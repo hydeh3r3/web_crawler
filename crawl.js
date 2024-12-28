428dce0688f050beb26aab6b329299dea85e1142
Hyde <wetbrain@Alis-MacBook-Pro.local>
2024-12-29 00:24:25 +0300

    new test cases and functionality

---
+++ crawl.js
@@ -0,0 +1,11 @@
+function normalizeUrl(urlString) {
+  const urlObj = new URL(urlString);
+  const hostpath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostpath.length > 0 && hostpath.slice(-1) === '/') {
+    return hostpath.slice(0, -1);
+  }
+  return hostpath;
+}
+
+module.exports = { normalizeUrl };
+ 
