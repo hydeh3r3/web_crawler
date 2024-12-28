bc9968108198dfe376ca7d83ce81af29945b0be7
Hyde <wetbrain@Alis-MacBook-Pro.local>
2024-12-29 01:53:44 +0300

    crawlpage recursive

--- crawl.js
+++ crawl.js
@@ -1,25 +1,45 @@
 const {JSDOM} = require('jsdom')
 
-async function crawlPage(baseURL) {
+async function crawlPage(baseURL,currentURL,pages) {
     // Ensure baseURL has a protocol
     const baseURLString = baseURL.startsWith('http') ? baseURL : `https://${baseURL}`
-    
-    console.log(`actively crawling: ${baseURLString}`)
+    const baseURLObj = new URL(baseURLString)
+    const currentURLObj = new URL(currentURL)
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeUrl(currentURL)
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
+
+    console.log(`actively crawling: ${currentURL}`)
+
     try {
         const resp = await fetch(baseURLString)
         if (resp.status > 399) {
             console.log(`Error with request: ${resp.status} on page: ${baseURLString}`)
-            return
+            return pages
         }
         const contentType = resp.headers.get('content-type')
         if (!contentType.includes('text/html')) {
             console.log(`non-html response: ${contentType} on page: ${baseURLString}`)
-            return
+            return pages
         }
-        console.log(await resp.text())
+        const htmlBody = await resp.text()
+        
+        const nextURLs = getURLsFromHTML(htmlBody, baseURLString)
+        for (const nextURL of nextURLs) {
+            pages = await crawlPage(baseURLString, nextURL, pages)
+        }
     } catch (err) {
         console.log(`Error in fetch: ${err.message}, on page: ${baseURLString}`)
     }
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
