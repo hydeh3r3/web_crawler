e6a063d60d39ff8e43549b3bd713c9819ba3cf8f
Hyde <wetbrain@Alis-MacBook-Pro.local>
2024-12-29 00:32:33 +0300

    new funct

--- crawl.js
+++ crawl.js
@@ -1,3 +1,17 @@
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const aElements = dom.window.document.querySelectorAll('a')
+    for (const aElement of aElements) {
+        if (aElement.href.slice(0, 1) === '/') {
+            urls.push(`${baseURL}${aElement.href}`)
+        } else {
+            urls.push(aElement.href)
+        }
+    }
+    return urls
+}
+
 function normalizeUrl(urlString) {
   const urlObj = new URL(urlString);
   const hostpath = `${urlObj.hostname}${urlObj.pathname}`;
